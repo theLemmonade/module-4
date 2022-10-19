76c00b4f7d4363a3291c6ead7f75555757371d08
theLemmonade <lemmonmail@icloud.com>
2022-10-18 21:56:54 -0400

    Commit 5: quiz debugged, timer bugged

--- script.js
+++ script.js
@@ -1,143 +1,143 @@
+var show = document.getElementById('show');
 var timer = document.getElementById('timer');
 var opening = document.getElementById('opening');
 var start = document.getElementById('start');
 var quiz = document.getElementById('quiz');
-var question = document.getElementById('question');
-var buttons = document.getElementById('answers')
-var initial = document.getElementById('initial').input
+var title = document.getElementById('title');
+var button0 = document.getElementById('0');
+var button1 = document.getElementById('1');
+var button2 = document.getElementById('2');
+var button3 = document.getElementById('3');
+var initial = document.getElementById('initial').input;
 var submit = document.getElementById('submit');
 var back = document.getElementById('back');
 var clear = document.getElementById('clear');
-var scoreboard = document.getElementById('scoreboard');
-var current = 0;
 var score = 0;
+var current = 0;
 var seconds = 80;
-//question pool
+
+//question pool with correct answer value, expandible
 var questionPool = [
   {question: "What does .css stand for?",
   answers: ["Concetrating Solar Solenoid", "Cascading Style Sheet", "Cat Super Star", "Cell Shaded Stats"],
-  correct: "Cascading Style Sheet",
+  correct: 1,
   },
   {question: "question content goes here",
   answers: ["answer1", "answer2", "answer3", "answer4"],
-  correct: "answer1",
+  correct: 0,
   },
   {question: "question content goes here",
   answers: ["answer1", "answer2", "answer3", "answer4"],
-  correct: "Answer4"
+  correct: 0,
   },
   {question: "question content goes here",
   answers: ["answer1", "answer2", "answer3", "answer4"],
-  correct: "Answer3"
+  correct: 0,
   },
   {question: "question content goes here",
   answers: ["answer1", "answer2", "answer3", "answer4"],
-  correct: "answer1",
+  correct: 0,
   },
 ]
+
 //stage .css display attributes
 quiz.setAttribute("style", "display:none");
 submit.setAttribute("style", "display:none");
 scoreboard.setAttribute("style", "display:none");
-//timer
+
+//timer, with gameover functionality
 function runTimer() {
     var timerInterval = setInterval (function() {
         seconds--;
         timer.textContent = seconds;
         if(seconds === 0) {
-            clearInterval(timerInterval);
-        }
+          console.log('Time out!');
+          score = 0;
+          clearInterval(timerInterval);
+          quiz.setAttribute("style", "display:none");
+          submit.setAttribute("style", "display:block");
+        };
     }, 1000);
 };
-//launch quiz
+
+//determines quiz progress and ends quiz with final question
 function runQuiz() {
-  console.log("Begin quiz with " + seconds + " seconds left.")  
-  opening.setAttribute("style", "display:none");
-  quiz.setAttribute("style", "display:block");
-  runTimer();
-  if (seconds === 0) {
-    console.log('Time out!');
-    score = "0";
-    clearInterval(timerInterval);
-    quiz.setAttribute("style", "display:none");
-    submit.setAttribute("style", "display:block");
-  } else if (current === 5){
-    console.log('You won! Your score is ' + seconds);
-    secondsleft = score;
-    clearInterval(timerInterval);
+  if (current === 0) {
+    console.log("Begin quiz with " + seconds + " seconds left.");
+    opening.setAttribute("style", "display:none");
+    quiz.setAttribute("style", "display:block");
+    runTimer();
+    drawQuiz();
+  } else if (current === questionPool.length) {
+    console.log("You won! Your score is " + seconds);
+    score = seconds;
     quiz.setAttribute("style", "display:none");
     submit.setAttribute("style", "display:block");
   } else {
-    for (var i = 0; i < questionPool.length; i++) {
-      var current = questionPool[i];
+    drawQuiz();
+  };
+};
+
+//draws the current question and answers from question pool
+function drawQuiz() {
+  console.log("Cycling question, current place is " + current + " out of " + questionPool.length + ".")
+  var currentQ = questionPool[current].question;
+  var currentA = questionPool[current].answers;
+  title.textContent = currentQ;
+  button0.textContent = currentA[0];
+  button1.textContent = currentA[1];
+  button2.textContent = currentA[2];
+  button3.textContent = currentA[3];
+};
+
+//decriment time for incorrect answer choice
+function checkAnswer() {
+  var choice = this.getAttribute("id");
+  console.log("User selected " + choice +", checking answer.");
+  correct = questionPool[current].correct;
+  if (choice == correct) {
+    console.log("Answer correct, cycling question");
+    current++;
+  } else {
+    console.log("Answer incorrect, deducting time, cycling question.");
+    seconds -= 10;
+    current++;
+  };
+  runQuiz();
+};
 
 
 
+//Submit inital and score to local storage
+function submitScore(){
+  console.log("Submitting initials and score to scoreboard.")
+  submit.setAttribute("style", "display:none");
+  scoreboard.setAttribute("style", "display:block");
+  //WIP
+};
 
+//function drawScore() {
+////WIP
+//};
 
+//function clearScore() {
+////WIP
+//};
 
-      
+//Reset quiz
+function reset() {
+  scoreboard.setAttribute("style", "display:none");
+  opening.setAttribute("style", "display:block");
+  score = 0;
+  current = 0;
+  seconds = 80;
+};
 
-
-    }
-  }
-
-
-
-
-  document.getElementById("start").addEventListener("click", runQuiz);
-
-  // if (secondsleft > 0) {
-//   console.log("Begin quiz with " + secondsleft + " seconds.")  
-//     function loadQuestion(){
-//         var currentQuestion = questionPool[current].question;
-//         questionEl.innerHTML = '';
-//         questionEl.innerHTML = currentQuestion;
-//         var currentAnswers = questionPool[current].answers;
-//         for (var i = 0; i <= currentAnswers.length -1; i += 1) {
-//           buttons[i].textContent = currentAnswers[i];
-//           buttons[i].addEventListener("click", checkAnswer);
-//         };
-//       };
-//       loadQuestion();
-//     function checkAnswer(){
-//           var choice = questionPool[current].answers[i],
-//               correct = questionPool[current].correct;
-//           if (choice === correct) {
-//             timePenalty(true);             
-//           } else {
-//             timePenalty(false);                        
-//           };  
-//           if (current < questionPool.length - 1) {
-//             console.log("Cycling question");
-//             current += 1;
-//             loadQuestion(current);
-//           } else {
-//             quiz.setAttribute("style", "display:none");
-//             submit.setAttribute("style", "display:block")
-//             console.log("Game over");
-//           };                  
-//         };
-//       function timePenalty(bool) {
-//         if (!bool) {
-//             timer -= 10
-//         };
-//       };
-//     };
-
-// //Add score to local storage
-// function submitScore(){
-//   submit.setAttribute("style", "display:none");
-//   scoreboard.setAttribute("style", "display:block");
-// }
-
-// //Reset Quiz
-// function reset(){
-//   scoreboard.setAttribute("style", "display:none");
-//   opening.setAttribute("style", "display:block");
-// }
-
-// //Navigate pages
-// start.addEventListener("click", beginQuiz);
-// submit.addEventListener("click", submitScore());
-// back.addEventListener("click", reset());
+start.addEventListener("click", runQuiz);
+button0.addEventListener("click", checkAnswer);
+button1.addEventListener("click", checkAnswer);
+button2.addEventListener("click", checkAnswer);
+button3.addEventListener("click", checkAnswer);
+submit.addEventListener("click", submitScore);
+back.addEventListener("click", reset);
+clear.addEventListener("click", ) //clearScore)
